748b5203455b8da6a6fb6d17dded13e69dd0f1be
brisa scomazzon <analuzespindolapagano@gmail.com>
2026-06-26 20:36:37 -0300

    continuestatement

--- homework3.js
+++ homework3.js
@@ -258,8 +258,19 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  let array = [];
+  let suma = numero;
 
+  for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+      continue; // Saltea esta vuelta y no hace el push
+    }
+    suma += 2;
+    array.push(suma);
+  }
+  return array;
 }
+
 
 function buscarIndice(array, elemento) {
   // Usa el método `.indexOf` para devolver el índice de "elemento" dentro de "array".
